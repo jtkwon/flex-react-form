87a49f86b96f41114b9623b26aa04696080fda96
jtkwon <jeff.t.kwon@gmail.com>
2022-03-22 17:34:20 -0400

    added a simple demo test; husky with eslint

--- packages/sample/src/App.test.js
+++ packages/sample/src/App.test.js
@@ -1,8 +1,20 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('When everything is OK', () => {
+  test('should render App component without crashing', () => {
+    render(<App />);
+  });
+
+  test('should check paaragraph', () => {
+    render(<App />);
+    screen.getByText('Test');
+    let error;
+    try {
+      expect(screen.getByText('Test')).toBeInTheDocument(); // explicit assertion
+    } catch (err) {
+      error = err;
+    }
+    expect(error).not.toBeDefined();
+  });
 });
